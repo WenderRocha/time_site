18221b51612ce567b8bf4777f4f798fcb5ea2cfc
Wender Rocha <47948529+WenderRocha@users.noreply.github.com>
2023-05-07 18:42:38 -0300

    finalizado layout do header, desktop e mobile

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+var menu = document.querySelector('nav ul');
+var menuBar = document.querySelector('nav .menu-icon');
+menuBar.addEventListener('click', function (){
+    menu.classList.toggle('active')
+})
